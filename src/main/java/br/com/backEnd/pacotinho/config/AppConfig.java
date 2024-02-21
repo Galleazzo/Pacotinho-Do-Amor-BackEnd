61e70bdf935c1030dd15f64676fd3ba4ef5a1fee
Galleazzo <paulogalleazzo1@gmail.com>
2024-02-20 21:21:55 -0300

    app config

---
+++ src/main/java/br/com/backEnd/pacotinho/config/AppConfig.java
@@ -0,0 +1,14 @@
+package br.com.backEnd.pacotinho.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
