1ca5c95e05a489ed350d08f5cf5906be5fed8844
Galleazzo <paulogalleazzo1@gmail.com>
2024-07-06 13:48:09 -0300

    mudança de consulta paginada e criação de

---
+++ src/main/java/br/com/backEnd/pacotinho/projections/AnimalsCriteria.java
@@ -0,0 +1,17 @@
+package br.com.backEnd.pacotinho.projections;
+
+import java.util.Date;
+
+public interface AnimalsCriteria {
+
+    Long getId();
+    String getName();
+    String getInstagramURL();
+    Date getRegistrationDate();
+    Long getPriority();
+    Boolean getActive();
+    byte[] getPicByte();
+    String getImageName();
+    String getImageType();
+
+}
